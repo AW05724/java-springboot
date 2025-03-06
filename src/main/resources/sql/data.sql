c3532c78c30f68b74ef55c34f4c26924650e5c40
Ayaka Wada <wada.ayaka@compass-japan.com>
2025-03-06 15:49:33 +0900

    課題２

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (id, name, mail) VALUES (1, '田中', 'tana@mail');
+INSERT INTO users (id, name, mail) VALUES (2, '佐藤', 'sato@mail');
+INSERT INTO users (id, name, mail) VALUES (3, '中村', 'mura@mail');

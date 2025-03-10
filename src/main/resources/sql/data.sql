dec298811162bfaa240c271270cdfad4da8a64cf
Ayaka Wada <wada.ayaka@compass-japan.com>
2025-03-10 16:46:13 +0900

    末尾に改行を追加、カラム変更

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,3 +1,8 @@
-INSERT INTO users (id, name, mail) VALUES (1, '田中', 'tana@mail');
-INSERT INTO users (id, name, mail) VALUES (2, '佐藤', 'sato@mail');
-INSERT INTO users (id, name, mail) VALUES (3, '中村', 'mura@mail');
+INSERT INTO `user` (mail, password, roles, created, last_logined, enabled)
+VALUES ('tana@mail', 'hashed_password_tana', 'ROLE_USER', '2025-01-31 10:00:00', NOW(), TRUE);
+
+INSERT INTO `user` (mail, password, roles, created, last_logined, enabled)
+VALUES ('sato@mail', 'hashed_password_sato', 'ROLE_ADMIN', '2025-03-01 10:00:00', NOW(), FALSE);
+
+INSERT INTO `user` (mail, password, roles, created, last_logined, enabled)
+VALUES ('mura@mail', 'hashed_password_mura', 'ROLE_EDITOR', NOW(), NOW(), TRUE);

69b0547c85cb5b0b52ea03aa0c2c83020bb417b7
Ayaka Wada <wada.ayaka@compass-japan.com>
2025-03-26 17:36:30 +0900

    0326進捗

--- src/main/java/com/example/demo/repository/mapper/User.java
+++ src/main/java/com/example/demo/repository/mapper/User.java
@@ -2,9 +2,11 @@ package com.example.demo.repository.mapper;
 
 import java.time.LocalDateTime;
 
-import lombok.Data;
+import lombok.Builder;
+import lombok.Getter;
 
-@Data
+@Getter
+@Builder
 public class User {
     private int id;
     private String mail;
@@ -13,60 +15,4 @@ public class User {
     private LocalDateTime created;
     private LocalDateTime lastLogined;
     private boolean enabled;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getMail() {
-        return mail;
-    }
-
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRoles() {
-        return roles;
-    }
-
-    public void setRoles(String roles) {
-        this.roles = roles;
-    }
-
-    public LocalDateTime getCreated() {
-        return created;
-    }
-
-    public void setCreated(LocalDateTime created) {
-        this.created = created;
-    }
-
-    public LocalDateTime getLastLogined() {
-        return lastLogined;
-    }
-
-    public void setLastLogined(LocalDateTime lastLogined) {
-        this.lastLogined = lastLogined;
-    }
-
-    public boolean getEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
 }

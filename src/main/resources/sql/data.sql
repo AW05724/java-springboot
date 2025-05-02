0702c66268a355ac5a4f552bd075f62c11d50ca8
Ayaka Wada <wada.ayaka@compass-japan.com>
2025-05-02 17:47:31 +0900

    不具合対応

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,8 +1,11 @@
-INSERT INTO `user` (mail, password, roles, created, last_logined, enabled)
+MERGE INTO `user` (mail, password, roles, created, last_logined, enabled)
+KEY(mail)
 VALUES ('tana@mail', 'hashed_password_tana', 'ROLE_USER', '2025-01-31 10:00:00', NOW(), TRUE);
 
-INSERT INTO `user` (mail, password, roles, created, last_logined, enabled)
+MERGE INTO `user` (mail, password, roles, created, last_logined, enabled)
+KEY(mail)
 VALUES ('sato@mail', 'hashed_password_sato', 'ROLE_ADMIN', '2025-03-01 10:00:00', NOW(), FALSE);
 
-INSERT INTO `user` (mail, password, roles, created, last_logined, enabled)
+MERGE INTO `user` (mail, password, roles, created, last_logined, enabled)
+KEY(mail)
 VALUES ('mura@mail', 'hashed_password_mura', 'ROLE_EDITOR', NOW(), NOW(), TRUE);
